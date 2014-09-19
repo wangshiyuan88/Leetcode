cc5f171c92c475cb02f1cf97cdc74b75f7e8e5f1
unknown <shiyuan.wang@SHIWANG-OP-W7.opnet.com>
2014-09-19 17:36:59 -0400

    9/19/2014

--- freq4/MedianOfTwoSortedArrays.java
+++ freq4/MedianOfTwoSortedArrays.java
@@ -7,37 +7,34 @@ public class MedianOfTwoSortedArrays {
 	public double findMedianSortedArrays(int A[], int B[]) {
         int totalLength = A.length+ B.length;
         if(totalLength%2==0)
-        	return (findMedianSortedArrays(totalLength/2-1, 0, A.length-1, A, 0, B.length-1, B) + findMedianSortedArrays(totalLength/2, 0, A.length-1, A, 0, B.length-1, B))/2;
+        	return (findMedianSortedArrays(totalLength/2, 0, A.length-1, A, 0, B.length-1, B) + findMedianSortedArrays(totalLength/2+1, 0, A.length-1, A, 0, B.length-1, B))/2;
         else
-        	return findMedianSortedArrays(totalLength/2, 0, A.length-1, A, 0, B.length-1, B);
+        	return findMedianSortedArrays(totalLength/2+1, 0, A.length-1, A, 0, B.length-1, B);
     }
 
 	private double findMedianSortedArrays(int target, int as, int ae, int[] A, int bs,
 			int be, int[] B) {
-		if(ae < as){
-			return B[bs+target];
-		}
-		if(be < bs)
-			return A[as+target];
-		if(target==0)
-			return A[as] < B[bs] ? A[as] : B[bs];
-		int am = (as+ae)/2;
-		int bm = (bs+be)/2;
-		int currentMaxIndex = am-as+1+bm-bs;
-		if(target>currentMaxIndex){
-			if(A[am]>B[bm])
-				return findMedianSortedArrays(target - (bm-bs+1), as, ae, A, bm+1, be, B);
-			else
-				return findMedianSortedArrays(target - (am-as+1), am+1, ae, A, bs, be, B);
-		}else{
-			if(A[am]>B[bm])
-				return findMedianSortedArrays(target-1, as, am-1, A, bs, be, B);
-			else
-				return findMedianSortedArrays(target-1, as, ae, A, bs, bm-1, B);
-		}
+		if(as>ae)
+			return B[bs+target-1];
+		if(bs>be)
+			return A[as+target-1];
+		if(target==1)
+			return A[as] > B[bs]? B[bs] : A[as];
+		int alength = ae-as+1;
+		int blength = be-bs+1;
+		if(blength<alength)
+			return findMedianSortedArrays(target, bs, be, B, as, ae, A);
+		int am = Math.min(alength, target/2);
+		int bm = target - am;
+		if(A[as+am-1]>B[bs+bm-1])
+			return findMedianSortedArrays(target-bm, as, ae, A, bs+bm, be, B);
+		else if(A[as+am-1]<B[bs+bm-1])
+			return findMedianSortedArrays(target-am, as+am, ae, A, bs, be, B);
+		return A[as+am-1];
 	}
 	
-	public static void main(String args[]){
-		new MedianOfTwoSortedArrays().findMedianSortedArrays(new int[]{1,2}, new int[]{1,2,3});
+
+	public static void main(String args[]){		
+		new MedianOfTwoSortedArrays().findMedianSortedArrays(new int[]{1,1}, new int[]{1,2});
 	}
 }
--- r/RegularExpressionMatching.java
+++ r/RegularExpressionMatching.java
@@ -5,7 +5,9 @@ public class RegularExpressionMatching {
 		  if(s.length()==0){
 			  return p.length()==0;
 		  }
-		  if(p.length() == 1 || p.charAt(1)!='*'){
+		  if(p.length()==0)
+			  return false;
+		  if(p.length() == 1 ||(p.length()>1&&p.charAt(1)!='*')){
 			  if(s.charAt(0)==p.charAt(0)||p.charAt(0)=='.'){
 				  return isMatch(s.substring(1), p.substring(1));
 			  }
@@ -14,13 +16,17 @@ public class RegularExpressionMatching {
 		  }
 		  else{
 			  int start = -1;
-			  while(start< s.length()&&(start < 0 || p.charAt(start) == '.' || s.charAt(start) == p.charAt(0))){
-				  if(isMatch(s.substring(start + 1), p.substring(2))){
+			  while(start< s.length()&&(start < 0 || p.charAt(0) == '.' || s.charAt(start) == p.charAt(0))){
+				  if(isMatch(s.substring(start+1), p.substring(2))){
 					  return true;
 				  }
-				  
+				  start++;
 			  }
 		  }
-	  return false;
+		  return false;
+	  }
+	  
+	  public  static void main(String []args){
+		  System.out.println(new RegularExpressionMatching().isMatch("aa", "a*"));
 	  }
 }
